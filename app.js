3ebee95eee90728ccbe98d349e6cb6516baf7fc5
Sigurd Kvamme <sigurdkvamme@live.no>
2022-04-06 21:11:13 +0200

    fixed error in app

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import express from "express";
 
 const app = express();
 
-app.route("/").get("/",(req,res)=>{
+app.route("/").get((req,res)=>{
   res.status(200).json({
     status: "sucess",
     message: "Hello!"
