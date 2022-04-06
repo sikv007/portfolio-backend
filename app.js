43f2857b18e157aede2d90ca8e6db492cb59f1a5
Sigurd Kvamme <sigurdkvamme@live.no>
2022-04-06 21:09:15 +0200

    created app and server

---
+++ app.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+
+app.route("/").get("/",(req,res)=>{
+  res.status(200).json({
+    status: "sucess",
+    message: "Hello!"
+  })
+})
+
+export default app;
