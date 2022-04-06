202e3fed609c708ad5b81fb8ee62975a377a9f3d
Sigurd Kvamme <sigurdkvamme@live.no>
2022-04-06 22:55:23 +0200

    fixed bug in app

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ import cors from "cors";
 const app = express();
 
 app.use(cors());
-app.use(express.static(`${__dirname}/static`));
+app.use(express.static("./static"));
 
 app.route("/").get((req, res) => {
   res.status(200).json({
